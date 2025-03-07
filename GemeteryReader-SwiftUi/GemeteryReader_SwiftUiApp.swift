33495fe4aba34435d50c61cdb1e9d69170cb7d47
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-07 20:13:15 +0530

    Xcode Update Changes

--- GemeteryReader-SwiftUi/GemeteryReader_SwiftUiApp.swift
+++ GemeteryReader-SwiftUi/GemeteryReader_SwiftUiApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GemeteryReader_SwiftUiApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            GeomertryReader2()
         }
     }
 }
--- GemeteryReader-SwiftUi/GeomertryReader2.swift
+++ GemeteryReader-SwiftUi/GeomertryReader2.swift
@@ -8,8 +8,85 @@
 import SwiftUI
 
 struct GeomertryReader2: View {
+    @State private var rotation: Double = 0
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        GeometryReader { gemetry in
+            
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.blue)
+                        .frame(width: 200,height: 600)
+                        .position(x: gemetry.size.width / 2, y: gemetry.size.height / 2)
+                        .rotationEffect(.degrees(rotation))
+                                        .animation(.easeIn(duration: 1.0), value: rotation)
+                        .onTapGesture {
+                            rotation += 45
+
+                        }
+                    
+                    VStack {
+                        Spacer()
+                        Circle()
+                            .fill(.red)
+                            .frame(width: 100, height: 100)
+                        
+                        Spacer()
+                        
+                        Circle()
+                            .fill(.red)
+                            .frame(width: 100, height: 100)
+                        
+                        Spacer()
+                        
+                        Circle()
+                            .fill(.red)
+                            .frame(width: 100, height: 100)
+                        
+                        Spacer()
+                    }
+                    
+                    
+                    Rectangle()
+                        .fill(.green)
+                        .frame(width: 380,height: 200)
+                        .position(x: gemetry.size.width / 2, y: gemetry.size.height / 2)
+                    
+                        .rotationEffect(.degrees(rotation))
+                                        .animation(.easeIn(duration: 1.0), value: rotation)
+                        .onTapGesture {
+                            rotation += 45
+
+                        }
+                    
+                    HStack {
+                        Spacer()
+                        Circle()
+                            .fill(.red)
+                            .frame(width: 100, height: 100)
+                        
+                        Spacer()
+                        
+                        Circle()
+                            .fill(.red)
+                            .frame(width: 100, height: 100)
+                        
+                        Spacer()
+                        
+                        Circle()
+                            .fill(.red)
+                            .frame(width: 100, height: 100)
+                        
+                        Spacer()
+                    }
+                    
+                    
+                }
+            }
+        }
     }
 }
 
