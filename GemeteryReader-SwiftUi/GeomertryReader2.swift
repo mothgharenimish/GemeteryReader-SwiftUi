1ad8953469940f383bf47c1ffbe67b31b119a6e3
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-07 20:12:48 +0530

    Xcode Update Changes

---
+++ GemeteryReader-SwiftUi/GeomertryReader2.swift
@@ -0,0 +1,18 @@
+//
+//  GeomertryReader2.swift
+//  GemeteryReader-SwiftUi
+//
+//  Created by Nimish Sharad Mothghare on 06/03/25.
+//
+
+import SwiftUI
+
+struct GeomertryReader2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeomertryReader2()
+}
